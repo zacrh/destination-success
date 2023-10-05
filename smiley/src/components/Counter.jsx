b48d707b11ee511e93d3e0e16c4d94d4511e2de6
Zachary Rosca-Halmagean <zac.princeton@gmail.com>
2023-10-04 19:47:07 -0500

    boiler plate done

--- smiley/src/components/Counter.jsx
+++ smiley/src/components/Counter.jsx
@@ -1,30 +1,67 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function KeyPressCounter() {
-  const [counters, setCounters] = useState({ arrow: 0, space: 0 });
+  const defaultCounters = {
+    ArrowLeft: { count: 0, img: '/good.png', emoji: '😊' },
+    ArrowUp: { count: 0, img: '/fine.png', emoji: '😐' },
+    ArrowRight: { count: 0, img: '/poor.png', emoji: '😞' },
+    ' ': { count: 0, img: '', emoji: '' },
+  };
+
+  const [flicker, setFlicker] = useState('');
+  const [counters, setCounters] = useState(
+    JSON.parse(localStorage.getItem('counters')) || defaultCounters,
+  );
 
   useEffect(() => {
-    const handleKeyPress = (e) => {
-      // Check if the key is an arrow key or the space bar
-      if(['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', ' '].includes(e.key)) {
-        setCounters(prevCounters => ({
-        ...prevCounters,
-        [e.key === ' ' ? 'space' : 'arrow']: prevCounters[e.key === ' ' ? 'space' : 'arrow'] + 1
-        }))
+    const handleKeyPress = (event) => {
+      if (Object.keys(counters).includes(event.key)) {
+        setCounters((prevCounters) => {
+          let newCounters = {
+            ...prevCounters,
+            [event.key]: {
+              ...prevCounters[event.key],
+              count: prevCounters[event.key].count + 1,
+            },
+          };
+          localStorage.setItem('counters', JSON.stringify(newCounters));
+
+          // Display flicker effect
+          if (event.key !== ' ') {
+            setFlicker(event.key);
+            setTimeout(() => setFlicker(''), 1000);
+          }
+          return newCounters;
+        });
       }
-    }
+    };
 
     window.addEventListener('keydown', handleKeyPress);
 
-    // Clean up the event listener when the component is unmounted
+    // Clean up the event listener when the component is destroyed
     return () => window.removeEventListener('keydown', handleKeyPress);
-  }, []);
+  }, [counters]);
 
   return (
     <div>
       <h2>Key Press Counter</h2>
-      <p>Arrow keys pressed: {counters.arrow}</p>
-      <p>Space bar pressed: {counters.space}</p>
+      <div>
+      {Object.entries(counters).map(([key, value]) =>
+  key !== " " ? (
+    <div key={key}>
+      <img
+        src={value.img}
+        alt=""
+        style={{
+          opacity: flicker === key ? "0.5" : "1",
+          transition: "opacity 1s",
+        }}
+      />{" "}
+      {value.emoji} Pressed: {value.count}
+    </div>
+  ) : null
+)}
+      </div>
     </div>
   );
 }
